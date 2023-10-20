7474a821ec67126f7a8bde073aaa068f99eb8e52
JanMaciuk <272664@student.pwr.edu.pl>
2023-10-20 15:19:16 -0400

    Implemented all tasks other than 6 and 8 in OCaml

---
+++ OCAml/main.ml
@@ -0,0 +1,76 @@
+print_endline "Hello World"
+let extractFromOption x = match x with
+  | Some i -> string_of_int i
+  | None -> "None";;
+  (*Wydobywam wartość z opcji*)
+
+  let optionToList optionValue = match optionValue with
+  | None -> []  
+  | Some lst -> lst 
+  (*Wydobywam listę z opcji*)
+
+  let rec print_int_list lst = match lst with
+  | [] -> ()  (* Pusta lista, pomijam *)
+  | head :: tail ->
+    let _ = Printf.printf "%d " head in print_int_list tail
+
+
+
+(*Zadanie 1*)
+let rec getLastElement list = match list with
+  | [] -> None
+  | [x] -> Some(x)
+  | _ :: tail -> getLastElement tail
+  (*przechodzę dalej aż do ostatniego elementu*)
+
+  let _ = print_endline (extractFromOption (getLastElement [1;2;3;4;5]))
+
+
+  (*Zadanie 2*)
+  let rec getTwoLastElements lista previousElement = match lista with
+  | [] -> None  (* Pusta lista *)
+  | [lastElement] -> (match previousElement with
+      | Some prevElem -> Some [prevElem; lastElement]
+      | None -> None) (*Lista jednoelementowa*)
+  | head :: tail -> getTwoLastElements tail (Some head)
+  (*Analogiczne do pierwszej, ale przenoszę przedostatni element do następnego wywołania*)
+
+  let _ = print_int_list (optionToList (getTwoLastElements [1;2;3;4;5] None))
+  let _ = print_endline ""
+
+  (*Zadanie 3*)
+  let rec listLength list : int = match list with
+  | [] -> 0
+  | head::tail -> 1 + (listLength tail)
+
+  let _ = print_endline (string_of_int (listLength [1;2;3;4;5]))
+
+  (*Zadanie 4*)
+  let rec listReverse list = match list with
+    | [] -> []
+    | head :: tail -> (listReverse tail) @ [head]
+
+  let _ = print_int_list (listReverse [1;2;3;4;5])
+  let _ = print_endline ""
+
+  (*Zadanie 5*)
+  let palindrome list = list = (listReverse list)
+    (*jeżeli lista jest identyczna z jej odwrotnością - jest palindromem*)
+
+  let _ = print_endline (string_of_bool (palindrome [1;2;3;4;5]))
+
+(*Zadanie 6*)
+(*TODO*)
+
+(*Zadanie 7*)
+
+let rec evenIndexesOnly list (evenIndex:bool) = match list with
+  | [] -> []
+  | head :: tail -> if evenIndex then head :: (evenIndexesOnly tail false) else evenIndexesOnly tail true
+  (*jeżeli jestem na parzystym indeksie to biorę element, jeżeli nie to pomijam*)
+
+let _ = print_int_list (evenIndexesOnly [1;2;3;4;5] true)
+let _ = print_endline ""
+
+(*Zadanie 8*)
+(*TODO*)
